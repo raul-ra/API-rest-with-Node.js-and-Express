2fa55728d161b5473aff1f1c924fd64b2864f806
raul-ra <raul.radillo@gmail.com>
2023-09-22 12:35:18 +0200

    added get and post functions

--- controller/productController.js
+++ controller/productController.js
@@ -1,58 +1,40 @@
-import data from '../data/data.json'assert { type: "json" }
 import productModel from "../models/productModel.js";
 
-async function getProducts(request,response){
-    try{
+
+//GET
+
+
+async function getProducts(request, response) {
+    try {
         const PRODUCTS = await productModel.getProducts();
-        res.json(PRODUCTS);
-} catch(err){
-    console.log(err);
-}
+        response.json(PRODUCTS);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-async function createProduct(request,response){
-    const body = request.body
-    data.push(body)
-    response.json(data)
-/* try{
-        const{title, description, image, price} = req.body
-        if (!title || !description || !image || !price) {
-            return res.status(400).json({error: "title, description, image and price are obligatory"}) 
-        }
-        const NEWPRODUCT = {title, description, image, price}
-        const PRODUCTS = await productModel.getProducts();
-        PRODUCTS.push(NEWPRODUCT)
-        await productModel.createProduct(NEWPRODUCT);
-        res.status(201).json(NEWPRODUCT);
-    } catch(error){
-        console.error("error", error);
-        res.status(500).json({error:'internal error'})
-    }*/
+//POST
+
+async function createProduct(request, response) {
+    try {
+        const body = request.body;
+        const newProduct = await productModel.createProduct(body);
+        response.json(newProduct);
+    } catch (error) {
+        console.error("Error:", error);
+        response.status(500).json({ error: 'Internal error' });
+    }
 }
+
+//PUT
+
 
 const productController = {
     getProducts,
     createProduct
-    };
+};
 
 export default productController;
 
 
 
-/*async function createProduct(req, res) {
-    try {
-        const { title, description, image, price } = req.body;
-        if (!title || !description || !image || !price) {
-            return res.status(400).json({ error: "title, description, image, and price are obligatory" });
-        }
-        const NEWPRODUCT = { title, description, image, price };
-        const PRODUCTS = await productModel.getProducts();
-        PRODUCTS.push(NEWPRODUCT);
-        await productModel.createProduct(NEWPRODUCT); // Corregir la función aquí
-        res.status(201).json(NEWPRODUCT); // Utilizar res en lugar de response
-    } catch (error) {
-        console.error("error", error);
-        res.status(500).json({ error: 'internal error' }); // Utilizar res en lugar de response
-    }
-}
-*/
--- models/productModel.js
+++ models/productModel.js
@@ -1,18 +1,19 @@
-import fs from 'fs';
-import{createRequire} from 'module';
+import {v4 as uuidv4}  from 'uuid';
+import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 const data = require('../data/data.json')
 
-async function getProducts(){
+async function getProducts() {
     return data;
 }
 
-
-async function createProduct (PRODUCTS){
-    try{ fs.writeFile (dataFilePath,json.stringify(PRODUCTS, null,2),"utf8")
-    } catch(error){throw error};
+async function createProduct(product) {
+    const newProduct = { id: uuidv4(), ...product };
+    data.push(newProduct);
+    return newProduct;
 }
+
 
 const productModel = {
     getProducts,
